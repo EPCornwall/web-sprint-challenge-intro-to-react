18cadd3b00bdd14e49f2f83d38c3780cd0fdcbc9
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-08-14 11:35:48 -0500

    mvp reached

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+import Ships from './Ships'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,34 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [shipData, setShipData] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://swapi.py4e.com/api/starships")
+    .then(res =>{
+      console.log(res)
+      setShipData(res.data.results)
+    })
+    .catch((error) =>{
+      console.log(error)
+    })
+  },[]);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Starships</h1>
+      {shipData.map((ship)=>{
+        return(
+        <Ships 
+        key = {ship.name}
+        name={ship.name}
+        model={ship.model}
+        cost={ship.cost_in_credits}
+        length={ship.length}
+        hyperDrive={ship.hyperdrive_rating}
+        />
+        );
+      })}
     </div>
   );
 }
